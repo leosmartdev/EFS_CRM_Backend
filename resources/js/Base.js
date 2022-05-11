1d611ccef1789f453222a4e8346d0cdde97b815b
leosultanov <Ilvirsultanovtchub21@gmail.com>
2022-05-12 01:08:20 +0300

    first commit

---
+++ resources/js/Base.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from './components/Header';
+
+const Base = ({ children }) => (
+  <div>
+    <Header />
+    <main>{children}</main>
+  </div>
+);
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.Auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Base);
---
+++ resources/js/routes/Public.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router';
+import Base from '../Base';
+
+const PublicRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (
+      <Base>
+        <Component {...props} />
+      </Base>
+    )}
+  />
+);
+
+PublicRoute.propTypes = {};
+
+export default PublicRoute;
